0e04d8b5118c0573ac1394b8cbf599a81ef62443
Nick Mikelsavage <nick@njsmdigital.com>
2025-04-05 18:26:55 -0400

    basic game state for cargo weight and money collected

--- global/signal_bus.gd
+++ global/signal_bus.gd
@@ -1,5 +1,14 @@
 extends Node
 
-# put signals in here
+# Money Collected Signals
+signal add_money_collected(moneyPaid: int)
+signal money_collected_updated()
 
+# Cargo Signals
+signal add_cargo()
+signal cargo_updated()
+signal cargo_at_max_capacity()
+signal sell_cargo()
+
+# Player Signals
 signal player_health_changed(new_health_value: int)
