ee65b5715e9e838131564e19e0371e2df280ca90
Andrew Michienzi <andrew@grcmc.org>
2025-04-05 17:00:59 -0400

    basic movement

--- scenes/submarine/submarine_body.gd
+++ scenes/submarine/submarine_body.gd
@@ -1,14 +1,25 @@
 extends CharacterBody2D
 @export var speed = 400
+@export var drill: DrillPosition
+@export var velocity_component: VelocityComponent
 
-func get_input():
-	var input_direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	velocity = input_direction * speed
-	print("Input" + str(input_direction))
+@onready var direction_input: Vector2 = Vector2.ZERO
 
-func _physics_process(delta):
-	get_input()
-	move_and_slide()
+func _process(delta: float) -> void:
+	direction_input = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+
+func _physics_process(delta: float):
+	apply_rotation()
+	
+	velocity_component.apply_move(direction_input, delta)
+	velocity_component.do_character_move(self)
+
+func apply_rotation() -> void:
+	var left_right_vector = Helpers.get_left_right_input_from_vector(direction_input)
+	if left_right_vector == Vector2.LEFT:
+		rotation_degrees = 0.0
+	elif left_right_vector == Vector2.RIGHT:
+		rotation_degrees = 180.0
 
 func _ready() -> void:
 	pass # Replace with function body.
