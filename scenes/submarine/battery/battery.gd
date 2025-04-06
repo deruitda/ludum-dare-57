dfa32bc6d2ba423ae7c726d6efb2a9a3d621616c
Andrew Michienzi <andrew@grcmc.org>
2025-04-06 15:14:10 -0400

    Update battery.gd

--- scenes/submarine/battery/battery.gd
+++ scenes/submarine/battery/battery.gd
@@ -7,7 +7,11 @@ class_name Battery
 func _ready() -> void:
 	fully_charge_battery()
 	SignalBus.battery_updated.emit(self)
+	SignalBus.purchase_completed.connect(_shop_item_purchased)
 
+func _shop_item_purchased(shop_item_resource: ShopItemResource):
+	if shop_item_resource.item_resource is BatteryResource:
+		upgrade_battery(shop_item_resource.item_resource)
 func has_enough_power_for(potential_power_amount: float) -> bool:
 	return current_power_level > potential_power_amount
 
@@ -22,6 +26,10 @@ func _set_current_power_level(power_amount: float) -> void:
 	SignalBus.battery_updated.emit(self)
 	if current_power_level == 0.0:
 		SignalBus.submarine_lost_power.emit()
+
+func upgrade_battery(_battery_resource: BatteryResource):
+	battery_resource = _battery_resource
+	fully_charge_battery()
 
 func consume_power(power_amount: float) -> void:
 	power_amount = max(current_power_level - power_amount, 0.0)
