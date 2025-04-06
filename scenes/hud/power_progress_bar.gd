836127f80b8b1ccab49de6deaa33e6392950a5dd
Andrew Michienzi <andrew@grcmc.org>
2025-04-06 03:29:48 -0400

    power bar blinking

--- scenes/hud/power_progress_bar.gd
+++ scenes/hud/power_progress_bar.gd
@@ -1,24 +1,22 @@
 extends ProgressBar
 @onready var STARTING_COLOR: Color = Color.GOLD
 @export var FLASH_COLOR: Color = Color.DARK_RED
-@export var battery_warning_threshold_percentage: float = 0.25
+@export var battery_warning_threshold_percentage: float = .25
 
 var tween: Tween = null
-@onready var flash_timer: Timer = $FlashTimer
 
 func _ready() -> void:
 	SignalBus.battery_updated.connect(_on_battery_updated)
 	modulate = STARTING_COLOR
 
+func _process(delta: float) -> void:
+	if value < battery_warning_threshold_percentage and not tween:
+		flash_progress_bar()
+			
+	
+
 func _on_battery_updated(battery: Battery):
 	value = battery.get_percentage_of_power_left()
-	if value < battery_warning_threshold_percentage:
-		flash_progress_bar()
-		if flash_timer.paused:
-			flash_timer.start()
-			
-	elif not flash_timer.paused:
-		flash_timer.stop()
 	
 func flash_progress_bar():
 	if tween:
@@ -27,14 +25,9 @@ func flash_progress_bar():
 	
 	modulate = FLASH_COLOR
 # Create a fresh tween each time to avoid invalid reference
-	var tween := create_tween()
-	tween.tween_property(self, "modulate", STARTING_COLOR, 0.2)
+	tween = create_tween()
+	tween.tween_property(self, "modulate", STARTING_COLOR, .9)
 	tween.connect("finished", tween_done)
 
 func tween_done():
 	tween = null
-
-
-func _on_flash_timer_timeout() -> void:
-	flash_progress_bar()
-	pass # Replace with function body.
