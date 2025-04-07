114766f197f27c501ceba3a8070cfce4163c1936
Wesley Reedy <wesley.reedy@gmail.com>
2025-04-07 01:09:49 -0400

    wave sounds

--- scenes/core/audio_controller.gd
+++ scenes/core/audio_controller.gd
@@ -2,6 +2,12 @@ extends Node2D
 
 @export var mute: bool = false
 @export var audio_stream_player: AudioStreamPlayer2D
+@onready var ambient_player: AudioStreamPlayer2D = $AmbientPlayer
+
+var surface_audio = [
+	preload("res://assets/audio/sfx/waves.wav")
+]
+
 func _ready():
 	if not mute:
 		play_music()
@@ -9,3 +15,8 @@ func _ready():
 func play_music():
 	if not mute:
 		audio_stream_player.play()
+
+func _process(delta: float) -> void:
+	if GameState.depth < 4 and !ambient_player.playing:
+		ambient_player.stream = surface_audio[0]
+		ambient_player.play()	
