acb0695abc03768cb3e4fb7cff268ed29ada4e03
Andrew Michienzi <andrew@grcmc.org>
2025-04-07 22:09:46 -0400

    moving logic to ping

--- global/ping_spawner.gd
+++ global/ping_spawner.gd
@@ -6,28 +6,7 @@ func _ready() -> void:
 	SignalBus.resource_pinged.connect(spawn_ping)
 	
 func spawn_ping(coords: Vector2, tile_resource: ValuableTileResource):
-	var camera := get_viewport().get_camera_2d()
-	if camera == null:
-		push_warning("No camera found")
-		return
-
 	var instance = ping_scene.instantiate() as Ping
 	instance.light_color = tile_resource.ping_color
-
-	var viewport_size = get_viewport().get_visible_rect().size
-	var half_view = viewport_size / 2.0
-	var camera_pos = camera.global_position
-
-	# Calculate screen bounds (world-space rect)
-	var top_left = camera_pos - half_view
-	var bottom_right = camera_pos + half_view
-	var screen_rect = Rect2(top_left, viewport_size)
-
-	if screen_rect.has_point(coords):
-		instance.global_position = coords
-	else:
-		# Clamp position to screen edges
-		var clamped_coords = coords.clamp(top_left, bottom_right)
-		instance.global_position = clamped_coords
-
+	instance.target_position = coords
 	get_tree().root.add_child(instance)
--- scenes/radar/ping.gd
+++ scenes/radar/ping.gd
@@ -6,12 +6,29 @@ class_name Ping
 @onready var timer: Timer = $Timer
 @onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
-@onready var _ping_global_position: Vector2
-
+@export var target_position: Vector2 = Vector2.ZERO
 var light_color: Color = Color.WHITE
 
 func _ready() -> void:
+	global_position = target_position
 	start_ping()
+
+func _physics_process(_delta: float) -> void:
+	var camera := get_viewport().get_camera_2d()
+	if camera == null:
+		return
+
+	var viewport_size = get_viewport().get_visible_rect().size
+	var half_view = viewport_size / 2.0
+	var camera_pos = camera.global_position
+	var top_left = camera_pos - half_view
+	var bottom_right = camera_pos + half_view
+	var screen_rect = Rect2(top_left, viewport_size)
+
+	if screen_rect.has_point(target_position):
+		global_position = target_position
+	else:
+		global_position = target_position.clamp(top_left, bottom_right)
 
 func _on_timer_timeout() -> void:
 	stop_ping()
@@ -23,4 +40,4 @@ func start_ping():
 	audio_stream_player_2d.play()
 	
 func stop_ping():
-	self.queue_free()
+	queue_free()
